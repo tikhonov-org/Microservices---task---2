5f0e20cdc1d93a3b24165220f8b4cb26d1827e8f
george <e.tihonov@websoft.ru>
2025-04-03 23:01:45 +0300

    Microservice task 2v1.1 Forgotten fix

--- src/main/java/ru/itmentor/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -21,7 +21,7 @@ public class SuccessUserHandler implements AuthenticationSuccessHandler {
         if (roles.contains("ROLE_ADMIN")) {
             httpServletResponse.sendRedirect("/admin");
         } else {
-            httpServletResponse.sendRedirect("/user/"+userDetails.getUser().getId());
+            httpServletResponse.sendRedirect("/user");
         }
     }
 }
